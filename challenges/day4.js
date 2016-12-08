5175ab4177c4ff1716a5f2bc1b91877209a9a19e
Alberto Coscia <alberto@albertocoscia.me>
2016-12-08 17:01:42 +0100

    Solutions for day 4

---
+++ challenges/day4.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = solve;
+
+function solve(input, challenge) {
+    if (challenge === 1) {
+        var letters  = {};
+        return input.split`\n`
+            .map(e => e.toUpperCase().split`-`.map((e, i, a) => {
+                if (i < a.length - 1) {
+                    for (let j = 0; j < e.length; j++) letters[e[j]] = (letters[e[j]] || 0) + 1;
+                    return 0;
+                } else {
+                    var c = Object.keys(letters).sort((a, b) => {
+                        if (letters[a] - letters[b] != 0) return letters[b]-letters[a];
+                        else return (a < b) ? -1 : (a > b) ? 1 : 0;
+                    }),
+                        sector = e.slice(0, 3),
+                        chksum = e.slice(4, 9);
+                    letters = {};
+                    if (c.join``.startsWith(chksum)) return +sector;
+                    else return 0;
+                }
+            }).reduce((a, b) => a+b)).reduce((a, b) => a+b);
+    } else {
+        var a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        return input.split`\n`
+            .map(e => e.toUpperCase().split`-`)
+            .map(e => {
+                var s = +e[e.length -1].slice(0, 3);
+                return e.map((f, i, o) => {
+                    var r = '';
+                    if (i < o.length -1) for (let j = 0; j < f.length; j++) r += a[(a.indexOf(f[j]) + s) % a.length]
+                    return (i != 0 && i != o.length -1? ' ' : '') + r;
+                }).join`` + ': ' + s
+            })
+            .filter(e => e.startsWith('NORTHPOLE OBJECT STORAGE'))
+            .join``
+    }
+}
