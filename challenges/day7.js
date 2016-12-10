3e4cf8d0897513d6dfc9f9c26bfc7b6739eee02d
Alberto Coscia <alberto@albertocoscia.me>
2016-12-10 15:20:52 +0100

    Solutions for 7.2

--- challenges/day7.js
+++ challenges/day7.js
@@ -2,18 +2,14 @@
 module.exports = solve;
 
 function solve(input, challenge) {
-    if (challenge === 1) {
-        return input.split`\n`
-            .map(e => e.split(/\[|]/g).reduce((a, b, i) => {
-                (i%2 ? a.h : a.nh).push(b);
-                return a;
-            }, {h: [], nh: []}))
-            .map(e => {
-                return e.nh.reduce((a, b) => /(.)(?!\1)(.)\2\1/.test(b) ? true : a, false) && !e.h.reduce((a, b) => /(.)(?!\1)(.)\2\1/.test(b) ? true : a, false);
-            })
-            .filter(e => e)
-            .length
-    } else {
+    let ma = (s) => {
+        let m = [];
+        if (/(.)(?!\1)(.)\1/.test(s)) m.push(s.match(/(.)(?!\1)(.)\1/));
+        while((s = s.substr(1)).length) if (/(.)(?!\1)(.)\1/.test(s)) m.push(s.match(/(.)(?!\1)(.)\1/));
+        return m.reduce((a, e) => (a.push(e[0][1] + e[0][0] + e[0][1]), a), []);
+    };
 
-    }
+    input = input.split`\n`.map(e => e.split(/\[|]/g).reduce((a, b, i) => ((i%2 ? a.h : a.nh).push(b), a), {h: [], nh: []}));
+    if (challenge === 1) return input.filter(e => e.nh.reduce((a, b) => (/(.)(?!\1)(.)\2\1/.test(b) || a), false) && !e.h.reduce((a, b) => (/(.)(?!\1)(.)\2\1/.test(b) || a), false)).length;
+    else return input.filter(e => ma(e.nh.join`\n`).some(aba => e.h.join`_`.indexOf(aba) != -1)).length;
 }
